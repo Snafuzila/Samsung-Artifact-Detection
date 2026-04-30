e6228272dc1fd641a69578bbc4b8a50f72351512
Snafuzila <boazattiya8@gmail.comm>
2026-04-30 20:32:39 +0300

    changed path to weights files

--- models/menual.py
+++ models/menual.py
@@ -20,8 +20,8 @@ def run_pipeline(image_path, threshold, iou, output_filename):
         return
 
     # Paths and Classes
-    BINARY_WEIGHTS = r"C:\Recovered\BOAZ\study\projects\samsung\weights\binary_model.pth"
-    CLASS_WEIGHTS = r"C:\Recovered\BOAZ\study\projects\samsung\weights\best_samsung_model.pth"
+    BINARY_WEIGHTS = r"Samsung-Artifact-Detection\weights\binary_model.pth"
+    CLASS_WEIGHTS = r"Samsung-Artifact-Detection\weights\best_samsung_model.pth"
     TEMP_JSON = "temp_suspects.json"
     SAMSUNG_CLASSES = ['Broken Line', 'Edge False Color', 'Over Desaturation', 'Saturated False Color', 'Smears']
 
